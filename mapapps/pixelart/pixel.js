429369749a82f315a3e8d16c4a8df6da3ec616bf
Daniel Johnson <dajohnson1978@gmail.com>
2017-08-16 11:46:03 -0500

    adding pixel art.

---
+++ mapapps/pixelart/pixel.js
@@ -0,0 +1,67 @@
+(function(){
+global.ctrl = {
+addColor: function(paletteId){
+},
+init:function() {
+    build(document.getElementsByTagName("body")[0], [{
+	       tag: "div",
+	       attrs:{
+	           id: "ctrls"
+	       },
+	       children: [{
+	       tag: "button",
+        attrs: { onClick: "ctrl.addColor('palette')" },
+        children: ["Add Color"]
+    },{
+        tag: "ol",
+        attrs: {
+            id: "palette"
+        },
+        children: []
+        },{
+        tag: "br"
+        },{
+        tag: "label",
+	       attrs:{
+	           for: "width"
+	       },
+	       children: [" Width: "]
+	       },{
+	       tag: "input",
+	       attrs:{
+	           id: "widthfield",
+	           name: "width",
+	           type: "number",
+	           value: 1,
+	           style: "width: 5em;",
+	           onChange: "resize('widthfield','heightfield')"
+	       }
+	       },{
+        tag: "label",
+	       attrs:{
+	           for: "height"
+	       },
+	       children: [" Height: "]
+	       },{
+	       tag: "input",
+	       attrs:{
+	           id: "heightfield",
+	           name: "height",
+	           type: "number",
+	           value: 1,
+	           style: "width: 5em;"
+	       }
+	       }]
+    },{
+        tag: "br"
+    },{
+        tag: "div",
+        attrs: { id: "canvas" }
+    }])
+}
+};
+)()
+
+
+
+
