ec6d8053a3db75295f49962d9bdfe2169995e8e9
Daniel Johnson <daniel.johnson@surescripts.com>
2017-08-23 10:08:01 -0500

    adding ajax page

---
+++ mapapps/ajax/ajax.js
@@ -0,0 +1,40 @@
+function buildRequest() {
+  var path = document.getElementById("path").value;
+  var body = document.getElementById("body").value;
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        window.open(this.response);
+      } else {
+        alert(this.responseText);
+      }
+    }
+  }
+  req.open("POST", "http://scullery-plateau.herokuapp.com/api" + path, true);
+  req.send(body);
+}
+
+function startup() {
+  build(document.getElementsByTagName("body")[0],[{
+    tag: "label",
+    children: [" Path: "]
+  },{
+		tag: "input",
+		attrs: {
+			id: "path",
+      type: "text"
+		}
+  },{ tag: "br" },{
+    tag: "textarea",
+    attrs: {
+      id: "body"
+    }
+  },{ tag: "br" },{
+    tag: "button",
+    attrs: {
+      onClick: "buildRequest()"
+    },
+    children: ["Submit"]
+  }]);
+}
