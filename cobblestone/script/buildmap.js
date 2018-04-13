4c70a6d808f6a1cd127af5a7174e882a004176b7
Daniel Johnson <dajohnson1978@gmail.com>
2018-04-13 15:48:36 -0500

    working state

--- cobblestone/script/buildmap.js
+++ cobblestone/script/buildmap.js
@@ -50,12 +50,12 @@ if (!Object.map) {
   }
 }
 (function() {
-	var transform = {
-		"flip-down":((p) => p.merge({ x:p.x, y:(15 - p.y) })),
-		"flip-over":((p) => p.merge({ x:(15 - p.x), y:p.y })),
-		"turn-left":((p) => p.merge({ x:p.y, y:(15 - p.x) })),
-		"turn-right":((p) => p.merge({ x:(15 - p.y), y:p.x }))
-	};
+  var transform = {
+    "flip-down":((p) => p.merge({ x:p.x, y:(15 - p.y) })),
+    "flip-over":((p) => p.merge({ x:(15 - p.x), y:p.y })),
+    "turn-left":((p) => p.merge({ x:p.y, y:(15 - p.x) })),
+    "turn-right":((p) => p.merge({ x:(15 - p.y), y:p.x }))
+  };
   
   var buildTransform = function(transforms) {
     return (p) => transforms.reduce((p1,f) => f(p1),p);
@@ -68,19 +68,8 @@ if (!Object.map) {
     }
   }
   
-  var drawSquare = function(coord,ctx) {
-    return function(point) {
-      var xy = ["x","y"].reduce(function(out, key){
-        out[key] = 6 * (point[key] + 16 * coord[key]);
-        return out;
-      },{});
-      ctx.fillStyle = point.c;
-      ctx.fillRect(xy.x, xy.y, 6, 6);
-    }
-  }
-  
   var explodeCoords = function(coords){
-    return coords.reduce(function(out,coord){
+    coords = coords.reduce(function(out,coord){
       var xy = coord.split("/");
       var x = xy[0].split("").map(decodeChar);
       var y = xy[1].split("").map(decodeChar);
@@ -88,23 +77,12 @@ if (!Object.map) {
       var xb = x[1] || xa;
       var ya = y[0];
       var yb = y[1] || ya;
-      return Array.range(ya,yb).reduce(function(out,y){
-        return out.concat(Array.range(xa,xb).map((x) => Object.map("x",x,"y",y)));
+      return Array.range(ya,yb+1).reduce(function(out,y){
+        return out.concat(Array.range(xa,xb+1).map((x) => Object.map("x",x,"y",y)));
       },[]);
     },[]);
+    return coords;
   }
-  
-  var applyToCtx = function(ctx, tiles, palettes) {
-    return function(placement) {
-      var tile = tiles[placement.shift()];
-      var palette = palettes[placement.shift()];
-      var transforms = placement.filter(Object.keys(transform).includes).map((k) => transform[k]);
-      tile = tile.map(buildTransform(transforms.concat(applyPalette(palette))));
-      explodeCoords(placement.slice(transforms.length)).forEach(function(coord) {
-        tile.forEach(drawSquare(coord, ctx));
-      });
-    }
-  };
   
   var safeparse = function(str) {
     var value = parseInt(str);
@@ -133,8 +111,8 @@ if (!Object.map) {
   
   var decodeChar = function (c) { return c.charCodeAt(0) - 97; }
   
-	var parseTile = function(tileText){
-		var explode = finishHex(tileText.split("|").reduce(function(out, row){
+  var parseTile = function(tileText){
+    var explode = finishHex(tileText.split("|").reduce(function(out, row){
       if (row == "") {
         var step = out.pop();
         out.push(step);
@@ -149,41 +127,89 @@ if (!Object.map) {
     return explode.reduce(function(out, row, y){
       return out.concat(row.map((c,x) => Object.map("c", decodeChar(c), "x", x, "y", y)));
     },[]);
-	};
-	
-	window.Builder = function (inId,canvasId,galleryId) {
-		var input = document.getElementById(inId);
-		var buildspace = document.getElementById(canvasId);
-		var gallery = document.getElementById(galleryId);
-		this.buildmap = function() {
-			buildspace.innerHTML = "";
-			gallery.innerHTML = "";
-			var inData = JSON.parse(input.value);
+  };
+  
+  var drawSquare = function(coord,ctx) {
+    return function(point) {
+      if (point.c != "none") {
+        var xy = ["x","y"].reduce(function(out, key){
+          out[key] = 6 * (point[key] + 16 * coord[key]);
+          return out;
+        },{});
+        ctx.fillStyle = point.c;
+        ctx.fillRect(xy.x, xy.y, 6, 6);
+      }
+    }
+  }
+  
+  var applyToCtx = function(ctx) {
+    return function(entry) {
+      entry.coords.forEach(function(coord) {
+        console.log("coord");
+        console.log(coord);
+        entry.tile.forEach(drawSquare(coord, ctx));
+        ctx.lineWidth = 1;
+        ctx.strokeRect(coord.x * 96, coord.y * 96, 96, 96);
+      });
+    }
+  };
+
+  window.Builder = function (inId,canvasId,galleryId) {
+    var input = document.getElementById(inId);
+    var buildspace = document.getElementById(canvasId);
+    var gallery = document.getElementById(galleryId);
+    this.buildmap = function() {
+      buildspace.innerHTML = "";
+      gallery.innerHTML = "";
+      console.log(input.value);
+      eval("var inData = (" + input.value + ");");
       console.log(inData);
-			var tiles = inData[0] || {};
-			tiles = Object.entries(tiles).reduce(function(out,entry){
-				out[entry[0]] = parseTile(entry[1]);
+      var tiles = inData[0] || {};
+      tiles = Object.entries(tiles).reduce(function(out,entry){
+        out[entry[0]] = parseTile(entry[1]);
         return out;
-			},{});
-			var palettes = inData[1] || {};
-      console.log(palettes);
-			palettes = Object.entries(palettes).reduce(function(out,entry){
-				out[entry[0]] = entry[1].map((c) => Array.isArray(c)?"rgb(" + c.join(",") + ")":c);
+      },{});
+      var palettes = inData[1] || {};
+      palettes = Object.entries(palettes).reduce(function(out,entry){
+        out[entry[0]] = entry[1].map((c) => Array.isArray(c)?"rgb(" + c.join(",") + ")":c);
         return out;
-			},{});
-      console.log(palettes);
-			var pages = inData[2] || {};
-      console.log(pages)
-			gallery.innerHTML = Object.entries(pages).map(function(entry){
-        console.log(entry);
-				var key = entry[0];
-				var page = entry[1];
-				buildspace.innerHTML += '<canvas id="' + key + '"></canvas>';
+      },{});
+      var pages = inData[2] || {};
+      gallery.innerHTML = Object.entries(pages).map(function(entry){
+        var key = entry[0];
+        var page = entry[1];
+        var exploded = page.reduce(function(out,step){
+          return Object.entries(step).reduce(function(o,entry){
+            var key = entry[0].split("|");
+            var tile = tiles[key.shift()];
+            var palette = palettes[key.shift()];
+            o.push({
+              tile:tile.map(buildTransform(key.map((k) => transform[k]).concat(applyPalette(palette)))),
+              coords:explodeCoords(entry[1])
+            });
+            return o;
+          },out)
+        },[]);
+        console.log("exploded");
+        console.log(exploded);
+        var coords = [].concat.apply([],exploded.map((x) => x.coords));
+        console.log(coords);
+        var max = coords.reduce(function(out,coord){
+          return ["x","y"].reduce(function(obj,key) {
+            obj[key] = Math.max(out[key],coord[key]);
+            return obj;
+          },{});
+        },{x:0,y:0});
+        console.log("max");
+        console.log(max);
+        buildspace.innerHTML += '<canvas id="' + key + '"></canvas>';
         var canvas = document.getElementById(key);
-        page.forEach(applyToCtx(canvas.getContext('2d'),tiles,palettes));
-				var img = canvas.toDataURL("image/png");
+        canvas.width = (1 + max.x) * 96;
+        canvas.height = (1 + max.y) * 96;
+        exploded.forEach(applyToCtx(canvas.getContext('2d'),tiles,palettes));
+        var img = canvas.toDataURL("image/png");
         return '<img src="' + img + '" alt="' + key + '"/>';
-			}).join("");
-		};
-	};
+      }).join("");
+    };
+  };
 })()
