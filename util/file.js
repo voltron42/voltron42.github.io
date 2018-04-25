c8434b86dabc1671a1d18aaee2dce29cd16aae7f
Daniel Johnson <dajohnson1978@gmail.com>
2018-04-24 23:02:38 -0500

    building out transformer

---
+++ util/file.js
@@ -0,0 +1,21 @@
+(function(){
+  window.loadFile = function(input,onComplete) {
+    var file = input.files[0];
+    if (!file) {
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      onComplete(e.target.result);
+    };
+    reader.readAsText(file);
+
+  }
+  window.makeDownloadLink = function(label,filename,type,encoding,data) {
+    var link = document.createElement("a");
+    link.innerHTML = label;
+    link.setAttribute("download",filename);
+    link.setAttribute("href","data:"+type+";"+encoding+","+encodeURI(data));
+    return link;
+  }
+})()
