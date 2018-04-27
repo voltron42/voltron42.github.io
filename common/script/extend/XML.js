27ef21a4650d570199fcc365a53c304bc4765450
Daniel Johnson <dajohnson1978@gmail.com>
2018-04-27 15:53:24 -0500

    updating work

--- common/script/extend/XML.js
+++ common/script/extend/XML.js
@@ -1,10 +1,29 @@
-if (!XMLDocument.prototype.toSimpleJSON) {
-  XMLDocument.prototype.toSimpleJSON = function() {
-    
+(function(){
+  if (!XMLDocument.prototype.toSimpleJSON) {
+    var toSimple = function(element) {
+      if ((typeof element) == "string") {
+        return element;
+      }
+      return {
+        tag:element.tagName,
+        attrs:element.getAttributeNames().reduce(function(out,name){
+          out[name] = element.getAttribute(name);
+          return out;
+        },{}),
+        content:Array.from(element.childNodes).map(toSimple)
+      };
+    }
+    XMLDocument.prototype.toSimpleJSON = function() {
+      return toSimple(this.documentElement);
+    }
   }
-}
-if (!XMLDocument.prototype.toObjectJSON) {
-  XMLDocument.prototype.toObjectJSON = function() {
-    
+  if (!XMLDocument.prototype.toObjectJSON) {
+    var toObject = function(element) {
+      
+    }
+    XMLDocument.prototype.toObjectJSON = function() {
+      var retval = toObject(this.documentElement);
+      return retval[this.documentElement.tagName];
+    }
   }
-}
+})()
