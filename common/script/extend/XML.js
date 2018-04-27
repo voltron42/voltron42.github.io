cc6cbc53aafdfd15616778e4c17616f0d8506697
Daniel Johnson <dajohnson1978@gmail.com>
2018-04-27 17:00:56 -0500

    tweeks

--- common/script/extend/XML.js
+++ common/script/extend/XML.js
@@ -1,4 +1,13 @@
 (function(){
+  var getAttrs = function(element) {
+    return element.getAttributeNames().reduce(function(out,name){
+      out[name] = element.getAttribute(name);
+      return out;
+    },{});
+  }
+  var mapChildren = function(element,func) {
+    return Array.from(element.childNodes).map(func)
+  }
   if (!XMLDocument.prototype.toSimpleJSON) {
     var toSimple = function(element) {
       if ((typeof element) == "string") {
@@ -6,11 +15,8 @@
       }
       return {
         tag:element.tagName,
-        attrs:element.getAttributeNames().reduce(function(out,name){
-          out[name] = element.getAttribute(name);
-          return out;
-        },{}),
-        content:Array.from(element.childNodes).map(toSimple)
+        attrs:getAttrs(element),
+        content:mapChildren(element,toSimple)
       };
     }
     XMLDocument.prototype.toSimpleJSON = function() {
@@ -18,8 +24,36 @@
     }
   }
   if (!XMLDocument.prototype.toObjectJSON) {
+    var makeMap = function(key,val) {
+      var out = {};
+      out[key] = val;
+      return out;
+    }
     var toObject = function(element) {
-      
+      if ((typeof node) == "string") {
+        return {"_text":element};
+      }
+      var out = Array.from(element.childNodes).map(toObject).reduce(function(out,child) {
+        return Object.entries(child).reduce(function(out,entry) {
+          var key = entry[0];
+          var val = entry[1];
+          if (!out[key]) {
+            out[key] = val;
+          } else {
+            out[key] = [].concat(out[key],val);
+          }
+          return out;
+        }, out);
+      }, getAttrs(element));
+      var keys = Object.keys(out);
+      if (keys.length == 1 && keys[0] == "_text" && (typeof out._text) == "string") {
+        out = out._text;
+      } else if (keys.length == 1 && keys[0] == "_text" && out._text.every(function(value) {
+          return (typeof value) == "string";
+        })) {
+        out = out._text.join("");
+      }
+      return makeMap(element.tagName, out);
     }
     XMLDocument.prototype.toObjectJSON = function() {
       var retval = toObject(this.documentElement);
