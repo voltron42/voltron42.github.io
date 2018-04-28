f90684dad91474af8aefae4315e5c5bebd9d75ce
Daniel Johnson <dajohnson1978@gmail.com>
2018-04-28 09:10:28 -0500

    building out testing and better error handling for domparser errors

--- common/script/extend/Array.test.js
+++ common/script/extend/Array.test.js
@@ -1,8 +1,13 @@
 describe("Array", function() {
   describe(".last", function() {
-    expect([1, 2, 3].last()).to.equal(3);
-    expect([3, 2, 1].last()).to.equal(1);
-    expect([6, 5, 4, 3, 2].last()).to.equal(2);
+    it("simple",function() {
+      expect([1, 2, 3].last()).to.equal(3);
+      expect([3, 2, 1].last()).to.equal(1);
+      expect([6, 5, 4, 3, 2].last()).to.equal(2);
+    })
+    it("empty",function() {
+      expect([].last()).to.be.undefined();
+    })
   })
   describe(".repeat", function() {
     
