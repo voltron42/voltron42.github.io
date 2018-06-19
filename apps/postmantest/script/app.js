588816449da4ffa1f8b0f11cdfdf2b4ccf20f090
Daniel Johnson <dajohnson1978@gmail.com>
2018-06-19 10:23:14 -0500

    fixing percentile calculation and print formatting, adding documentation and title

--- apps/postmantest/script/app.js
+++ apps/postmantest/script/app.js
@@ -8,19 +8,18 @@
       var average = function(values) {
         return values.reduce((a,b) => a + parseInt(b), 0) / values.length;
       }
-      var percentile = function(p, arr) {
+      var percentile = function(p, list) {
+        var arr = Array.from(list).map((a) => a);
+        arr.sort();
         if (arr.length === 0) return 0;
         if (typeof p !== 'number') throw new TypeError('p must be a number');
         if (p <= 0) return arr[0];
         if (p >= 1) return arr[arr.length - 1];
 
         var index = arr.length * p,
-            lower = Math.floor(index),
-            upper = lower + 1,
-            weight = index % 1;
+            lower = Math.floor(index);
 
-        if (upper >= arr.length) return arr[lower];
-        return arr[lower] * (1 - weight) + arr[upper] * weight;
+        return arr[lower];
       }
       var initColors = [
         'lightgrey',
