b0a309989e6cb7b04a175a501a6f17d7329ad7e9
Daniel Johnson <dajohnson1978@gmail.com>
2018-12-17 01:16:33 -0600

    working state

--- poc/cascade/script/poc.js
+++ poc/cascade/script/poc.js
@@ -7,10 +7,11 @@
       return prefix + retval;
     };
   }
-  window.CascadeController = function(instanceName,ctrlId,srcCls,tgtCls) {
+  window.CascadeController = function(instanceName,ctrlId,srcCls,tgtCls,menuId,menuOptions) {
     var optSeq = new Sequence("opt-",0);
     var tgtSeq = new Sequence("tgt-",0);
     var tree = [];
+    var stack = {};
     this.init = function() {
       var firstTargetId = tgtSeq.getNextId();
       tree.push(firstTargetId);
@@ -22,50 +23,80 @@
         throw msg;
       }
       targetList[0].id = firstTargetId;
-      Array.from(document.getElementsByClassName(srcCls)).map(function(srcElem){
-        srcElem.id = optSeq.getNextId();
-      });
+      document.getElementById(menuId).innerHTML = "<ul>" + menuOptions.map(function(opt){
+        return '<li><a id="' + optSeq.getNextId() + '" class="option" draggable="true" ondragstart="ctrl.drag(event)">' + opt + '</a></li>'
+      }).join("") + "</ul>";
+    }
+    var getTarget = function(elem) {
+      
+      var tgt = elem;
+      while (!tgt.classList.contains("tree") && tgt.nodeName.toLowerCase() != "body") {
+        tgt = tgt.parentElement;
+      }
+      return tgt;
     }
     this.allowDrop = function(e) {
+      var tgt = getTarget(e.target);
+      if (!stack[tgt.id]) {
+        console.log("over");
+        console.log(tgt);
+        stack[tgt.id] = true;  
+        console.log(stack);
+      }
+      e.preventDefault();
+    };
+    this.disallowDrop = function(e) {
+      console.log("and out");
+      delete stack[e.target.id];
       e.preventDefault();
     };
     this.drag = function(e) {
-      console.log(e);
+      console.log("drag");
+      console.log(e.target);
       e.dataTransfer.setData(ctrlId, e.target.id);
     };
-    this.drop = function(e,id) {
+    this.drop = function(e) {
       e.preventDefault();
       var data = e.dataTransfer.getData(ctrlId);
       var src = document.getElementById(data);
-      var dest = e.target;
+      var dest = getTarget(e.target);
       console.log("dropping " + data);
       console.log(src);
       console.log(dest);
       console.log(src.nodeName);
-      switch(src.nodeName) {
-        case "A":
-          var label = src.innerHTML;
-          var node = document.createElement("ul");
-          node.id = tgtSeq.getNextId();
-          node.innerHTML = "<li><a>" + label + "</a></li>";
-          node.classList.add(tgtCls);
-          node.addEventListener("ondragover", this.allowDrop);
-          node.addEventListener("ondrop", this.drop);
-          var item = document.createElement("li");
-          item.id = optSeq.getNextId();
-          item.classList.add(srcCls);
-          item.draggable = true;
-          item.addEventListener("ondragstart", this.drag);
-          item.appendChild(node);
-          dest.appendChild(item);
-          break;
-        case "LI":
-          dest.appendChild(src);
-          break;
-        default:
-          var msg = "not dropable";
-          alert(msg);
-          throw msg;
+      console.log(stack);
+      var keys = Object.keys(stack);
+      var children = keys.map(function(id){
+        return document.getElementById(id);
+      }).filter(function(elem) {
+        return elem.id != dest.id && dest.contains(elem);
+      });
+      console.log(children);
+      if (keys.length > 0 && children.length == 0) {
+        switch(src.nodeName) {
+          case "A":
+            var label = src.innerHTML;
+            var tgtId = tgtSeq.getNextId();
+            var optId = optSeq.getNextId();
+            var html = '<li id="' + optId + 
+            '" class="option" draggable="true" ondragstart="' + instanceName + 
+            '.drag(event)"><ul id="' + tgtId + '" class="tree" ondragover="' + instanceName + 
+            '.allowDrop(event)" ondrop="' + instanceName + '.drop(event)" ondragleave="' + 
+            instanceName + '.disallowDrop(event)"><li><a>' + label + "</a></li></ul></li>";
+            dest.innerHTML += html;
+            break;
+          case "LI":
+            if (dest.nodeName.toUpperCase() == "BODY") {
+              src.parentElement.removeChild(src);
+            } else {
+              dest.appendChild(src);
+            }
+            break;
+          default:
+            var msg = "not dropable";
+            alert(msg);
+            throw msg;
+        }
       }
     };
   };
