29753e1fe5a8df297acf53bf5fd1a8b05246057e
voltron42 <voltron42@users.noreply.github.com>
2022-09-22 15:53:16 -0500

    bug fix

--- gizmo-atheneum/structure/importnamespace/mock.js
+++ gizmo-atheneum/structure/importnamespace/mock.js
@@ -1,26 +1,26 @@
-(function(){
+(function () {
   global = global || window;
   let register = {};
-  let oldImport = import;
-  global.import = function(name,depObj,allowImports) {
+  let oldImport = global.import;
+  global.import = function (name, depObj, allowImports) {
     depObj = depObj || {};
     if (!(name in register)) {
-      throw new Error(name + " invalid service name");
+      throw new Error(name + ' invalid service name');
     }
     let service = register[name];
-    let fullDeps = Object.entries(service.dependencies).reduce((out,entry) => {
-      if ((typeof out) == "string") {
+    let fullDeps = Object.entries(service.dependencies).reduce((out, entry) => {
+      if (typeof out == 'string') {
         return out;
       } else if (depObj[entry[0]]) {
         out[entry[1]] = depObj[entry[0]];
       } else if (allowImports) {
         out[entry[1]] = oldImport(entry[0]);
       } else {
-        return "Missing mocked namespace: " + entry[0];
+        return 'Missing mocked namespace: ' + entry[0];
       }
       return out;
     }, {});
     let retVal = service.nsBuilder(fullDeps);
     return retVal;
-  }
+  };
 })();
