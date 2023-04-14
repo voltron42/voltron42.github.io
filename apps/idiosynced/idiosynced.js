34c89b7c38c4d4f8fae5d7be164655855446d01d
Daniel Johnson <voltron42@users.noreply.github.com>
2023-04-14 22:25:14 +0000

    stub for idiosynced POC

---
+++ apps/idiosynced/idiosynced.js
@@ -0,0 +1,11 @@
+namespace("voltron42.Idiosynced",{},() => {
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {};
+        }
+        render() {
+            return <>Hello</>;
+        }
+    }
+});
