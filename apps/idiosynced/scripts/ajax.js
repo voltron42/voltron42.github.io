11f39fd6ca018536febd05528a381b49c48d200c
Daniel Johnson <voltron42@users.noreply.github.com>
2023-04-24 16:53:09 +0000

    adding ajax

---
+++ apps/idiosynced/scripts/ajax.js
@@ -0,0 +1,60 @@
+namespace("v42.idiosynced.Ajax",{},() => {
+    const callbackEvents = {
+      success: 'AjaxSuccess',
+      failure: 'AjaxFailure',
+      stateChange: 'AjaxStateChanged',
+    };
+    const defaultCallbacks = Object.entries(callbackEvents).reduce(
+      (out, [k, v]) => {
+        out[k] = (arg) => {
+          document.dispatchEvent(new CustomEvent(v, { detail: arg }));
+        };
+        return out;
+      },
+      {}
+    );
+    const request = function(method, filepath, payload, callbacks) {
+        [payload, callbacks] = callbacks?[payload,callbacks]:[undefined,payload];
+        if (typeof callbacks == 'function') {
+            callbacks = {
+            success: callbacks,
+            };
+        }
+        callbacks = Object.entries(callbacks).reduce((out, [k, v]) => {
+            out[k] = v;
+            return out;
+        }, defaultCallbacks);
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4) {
+            if (this.status === 200) {
+                callbacks.success(this.responseText);
+            } else {
+                if (callbacks.failure) {
+                    callbacks.failure({
+                        requestedFile: filepath,
+                        status: this.status,
+                        statusText: this.statusText,
+                        responseText: this.responseText,
+                    });
+                }
+            }
+            } else {
+                if (callbacks.stateChange) {
+                    callbacks.stateChange({
+                        state: this.readyState,
+                        min: 0,
+                        max: 4,
+                    });
+                }
+            }
+        };
+        xhttp.open(method, filepath, true);
+        if (payload) {
+            xhttp.send(payload);
+        } else {
+            xhttp.send();
+        }
+    };
+    return { getLocalStaticFileAsText }
+  });
---
+++ apps/idiosynced/scripts/googleAPI.js
@@ -0,0 +1,3 @@
+namespace("v42.idiosynced.GoogleAPI",{},() => {
+    
+})
