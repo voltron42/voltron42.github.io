68b974546c032e21aee38bdc884a9b5fa4557fa4
Voltron42 <dajohnson1978@gmail.com>
2023-05-24 09:57:04 -0500

    building out minesweeper

---
+++ catchpenny-colonade/minesweeper/minesweeper.js
@@ -0,0 +1,16 @@
+namespace("minesweeper.Minesweeper",{},() => {
+    const icons = {
+        "explosion":<i class="fa-duotone fa-sun" style="--fa-secondary-color: #ff0000; --fa-secondary-opacity: 1;"></i>,
+        "bomb":<></>,
+        "flag":<></>,
+        "smile":<></>
+    };
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+        }
+        render() {
+            return <></>;
+        }
+    }
+});
