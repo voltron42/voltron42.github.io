a02fd6cf4aed69f663b65598f3ff4cc2980fdd90
Voltron42 <dajohnson1978@gmail.com>
2023-06-02 09:18:02 -0500

    working state

--- catchpenny-colonnade/snake/snake.js
+++ catchpenny-colonnade/snake/snake.js
@@ -1,4 +1,6 @@
-namespace('snake.Snake',{},() => {
+namespace('snake.Snake',{
+    'minesweeper.DigitalDisplay':'DigitalDisplay'
+},({ DigitalDisplay }) => {
     const config = {
         cellSize: 20,
         head:{
@@ -11,8 +13,8 @@ namespace('snake.Snake',{},() => {
             }
         },
         colors: {
-            bg: "green",
-            snake: "blue",
+            bg: "darkblue",
+            snake: "lightgreen",
             apple: "red"
         }
     };
@@ -22,10 +24,28 @@ namespace('snake.Snake',{},() => {
         })
     }
     const directions = {
-        right: [1, 0, "left"],
-        left: [-1, 0, "right"],
-        up: [0, -1, "down"],
-        down: [0, 1, "up"],
+        none: [0,0,{
+            up: true,
+            down: true,
+            left: true,
+            right: true
+        }],
+        right: [1, 0, {
+            up: true,
+            down: true
+        }],
+        left: [-1, 0, {
+            up: true,
+            down: true
+        }],
+        up: [0, -1, {
+            left: true,
+            right: true
+        }],
+        down: [0, 1, {
+            left: true,
+            right: true
+        }],
     }
     const directionKeys = {
         "ArrowUp":"up",
@@ -73,13 +93,17 @@ namespace('snake.Snake',{},() => {
         return { snake, direction: startingDirection }
     }
     const getSnakeHead = function(directionKey) {
-        console.log({ directionKey });
-        return matrixMath(config.head.points,config.head.matricies[directionKey]);
+        if (directionKey === "none") {
+            directionKey = "down";
+        }
+        const matrix = config.head.matricies[directionKey];
+        console.log({ directionKey, matrix });
+        return matrixMath(config.head.points, matrix);
     }
     return class extends React.Component {
         constructor(props) {
             super(props);
-            const [ width, height, snakeLength, timeDelay] = [ 10, 10, 5, 500 ];
+            const [ width, height, snakeLength, timeDelay] = [ 20, 12, 5, 500 ];
             this.coords = buildCoords(width, height)
             const { snake, direction } = buildSnake(width, height, this.coords, snakeLength);
             const apple = placeApple(this.coords, snake);
@@ -91,16 +115,19 @@ namespace('snake.Snake',{},() => {
                 timeDelay,
                 direction,
                 snake,
-                apple
+                apple,
+                appleCount: 0
             };
             document.addEventListener("keyhold",(({ detail }) => {
-                console.log({detail});
-                const direction = directionKeys[detail.code];
-                if (direction) {
-                    if (this.timeout) {
-                        clearTimeout(this.timeout);
+                if (!this.state.endGame) {
+                    console.log({detail});
+                    const direction = directionKeys[detail.code];
+                    if (direction && directions[this.state.direction][2][direction]) {
+                        if (this.timeout) {
+                            clearTimeout(this.timeout);
+                        }
+                        this.step(direction);
                     }
-                    this.setState({ direction });
                 }
             }));
         }
@@ -110,39 +137,43 @@ namespace('snake.Snake',{},() => {
         componentDidMount(){
             this.afterRender();
         }
+        step(direction) {
+            let { appleCount, apple, timeDelay } = this.state;
+            const { width, height } = this.state 
+            const snake = Array.from(this.state.snake);
+            const [dx,dy] = directions[direction];
+            const [x0,y0] = snake[0];
+            const newHead = [x0+dx,y0+dy];
+            snake.unshift(newHead);
+            let endGame = false;
+            if (newHead[0] < 0 || newHead[0] >= width || newHead[1] < 0 || newHead[1] >= height) {
+                endGame = true;
+            } else if (snake.slice(1).filter(([x1,y1]) => {
+                return x1 === newHead[0] && y1 === newHead[1];
+            }).length > 0) {
+                endGame = true;
+            }
+            if (endGame) {
+                if (!this.state.endGame) {
+                    this.setState({ endGame });
+                }
+            } else { 
+                if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
+                    timeDelay-=10;
+                    apple = placeApple(this.coords,snake);
+                    appleCount++;
+                } else {
+                    snake.pop();
+                }
+                this.setState({ apple, appleCount, timeDelay, snake, direction });
+            }
+        }
         afterRender() {
             if (this.state.endGame) {
                 alert("GAME OVER!")
             } else {
                 this.timeout = setTimeout(() => {
-                    let { apple, timeDelay } = this.state;
-                    const { width, height, direction } = this.state 
-                    const snake = Array.from(this.state.snake);
-                    const [dx,dy] = directions[direction];
-                    const [x0,y0] = snake[0];
-                    const newHead = [x0+dx,y0+dy];
-                    snake.unshift(newHead);
-                    let endGame = false;
-                    if (newHead[0] < 0 || newHead >= width || newHead[1] < 0 || newHead[1] >= height) {
-                        endGame = true;
-                    } else if (snake.slice(1).filter(([x1,y1]) => {
-                        return x1 === newHead[0] && y1 === newHead[1];
-                    }).length > 0) {
-                        endGame = true;
-                    }
-                    if (endGame) {
-                        if (!this.state.endGame) {
-                            this.setState({ endGame });
-                        }
-                    } else { 
-                        if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
-                            timeDelay-=10;
-                            apple = placeApple(this.coords,snake);
-                        } else {
-                            snake.pop();
-                        }
-                        this.setState({ apple, timeDelay, snake });
-                    }
+                    this.step(this.state.direction);
                 }, this.state.timeDelay);
             }
         }
@@ -153,8 +184,12 @@ namespace('snake.Snake',{},() => {
             const [hx,hy] = this.state.snake[0];
             return <div className="d-flex flex-column justify-content-center">
                 <h1 className="text-center">Snake</h1>
-                <div className="d-flex justify-content-center">
-                    <svg width="60%" height="60%" viewBox={`0 0 ${fullWidth} ${fullHeight}`}>
+                <div className="d-flex justify-content-between">
+                    <DigitalDisplay digitCount={3} value={this.state.appleCount}/>
+                    <DigitalDisplay digitCount={3} value={this.state.timeDelay}/>
+                </div>
+                <div className="d-flex justify-content-center" style={{ height: innerHeight * 0.75}}>
+                    <svg width="100%" height="100%" viewBox={`0 0 ${fullWidth} ${fullHeight}`}>
                         <rect width={fullWidth} height={fullHeight} fill={config.colors.bg} stroke="none"/>
                         <polyline points={this.state.snake.map(([x,y]) => coordKey(x * config.cellSize + halfCell, y * config.cellSize + halfCell )).join(" ")} stroke={config.colors.snake} strokeWidth={halfCell} strokeLinecap="round" fill="none"/>
                         <circle cx={this.state.apple[0] * config.cellSize + halfCell} cy={this.state.apple[1] * config.cellSize + halfCell} r={halfCell} fill={config.colors.apple} stroke="none"/>
