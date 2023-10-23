d494b711e2ee367190e8ef732471005088be3485
Voltron42 <dajohnson1978@gmail.com>
2023-10-23 15:10:23 -0500

    fixing key event wrapper

--- gizmo-atheneum/util/keyeventwrapper.js
+++ gizmo-atheneum/util/keyeventwrapper.js
@@ -21,24 +21,34 @@
   };
 
   const getKeyState = ((detail) => {
-    return detail.altKey?"alt":"" + detail.ctrlKey?"ctrl":"" + detail.shiftKey?"shift":"";
+    return (detail.altKey?"alt":"") + (detail.ctrlKey?"ctrl":"") + (detail.shiftKey?"shift":"");
   });
+
+  const stateKeys = {
+    "altAlt": true,
+    "shiftShift": true,
+    "ctrlControl": true,
+  };
 
   const handleKeyEvent = ((e) => {
     const detail = keyEventFields.reduce((out,field) => {
       out[field] = e[field];
       return out;
     },{});
+    detail.preventDefault = (() => {
+      e.preventDefault();
+    });
     if (detail.type in eventMap) {
       const mapped = eventMap[detail.type]
       if (keyHolds[detail.keyCode] != mapped.hold) {
         const keyState = getKeyState(detail);
         const keyEvent = detail.code.toLowerCase() + mapped.suffix;
+        const keyStateEvent = detail.code.toLowerCase() + keyState + mapped.suffix;
         document.dispatchEvent(new CustomEvent(mapped.event, { detail }));
         document.dispatchEvent(new CustomEvent(keyEvent, { detail }));
-        if (keyState.length > 0 && keyState != detail.key.toLowerCase() && !(keyState == "ctrl" && detail.key == "Control")) {
+        if (keyState.length > 0 && !stateKeys[keyState + detail.key]) {
           document.dispatchEvent(new CustomEvent(mapped.event + keyState, { detail }));
-          document.dispatchEvent(new CustomEvent(keyEvent + keyState, { detail }));
+          document.dispatchEvent(new CustomEvent(keyStateEvent, { detail }));
         }
       }
       keyHolds[detail.keyCode] = mapped.hold;
