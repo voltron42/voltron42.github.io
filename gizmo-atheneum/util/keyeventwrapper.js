6858b27f6a39c2bb910c92e66ddcba69a507b213
Voltron42 <dajohnson1978@gmail.com>
2023-10-24 07:39:46 -0500

    wrapping original key events as well

--- gizmo-atheneum/util/keyeventwrapper.js
+++ gizmo-atheneum/util/keyeventwrapper.js
@@ -20,6 +20,12 @@
     }
   };
 
+  const suffixes = {
+    "keyup":"up",
+    "keydown":"down",
+    "keypress":"press"
+  }
+
   const getKeyState = ((detail) => {
     return (detail.altKey?"alt":"") + (detail.ctrlKey?"ctrl":"") + (detail.shiftKey?"shift":"");
   });
@@ -30,6 +36,17 @@
     "ctrlControl": true,
   };
 
+  const wrapKeyEvent = ((mapped, detail) => {
+    const keyState = getKeyState(detail);
+    const keyEvent = detail.code.toLowerCase() + mapped.suffix;
+    const keyStateEvent = detail.code.toLowerCase() + keyState + mapped.suffix;
+    document.dispatchEvent(new CustomEvent(keyEvent, { detail }));
+    if (keyState.length > 0 && !stateKeys[keyState + detail.key]) {
+      document.dispatchEvent(new CustomEvent(mapped.event + keyState, { detail }));
+      document.dispatchEvent(new CustomEvent(keyStateEvent, { detail }));
+    }
+  });
+
   const handleKeyEvent = ((e) => {
     const detail = keyEventFields.reduce((out,field) => {
       out[field] = e[field];
@@ -38,18 +55,15 @@
     detail.preventDefault = (() => {
       e.preventDefault();
     });
+    wrapKeyEvent({
+      event: detail.type,
+      suffix: suffixes[detail.type]
+    }, detail);
     if (detail.type in eventMap) {
       const mapped = eventMap[detail.type]
       if (keyHolds[detail.keyCode] != mapped.hold) {
-        const keyState = getKeyState(detail);
-        const keyEvent = detail.code.toLowerCase() + mapped.suffix;
-        const keyStateEvent = detail.code.toLowerCase() + keyState + mapped.suffix;
         document.dispatchEvent(new CustomEvent(mapped.event, { detail }));
-        document.dispatchEvent(new CustomEvent(keyEvent, { detail }));
-        if (keyState.length > 0 && !stateKeys[keyState + detail.key]) {
-          document.dispatchEvent(new CustomEvent(mapped.event + keyState, { detail }));
-          document.dispatchEvent(new CustomEvent(keyStateEvent, { detail }));
-        }
+        wrapKeyEvent(mapped, detail);
       }
       keyHolds[detail.keyCode] = mapped.hold;
     }
