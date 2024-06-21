0331da32d2ccddeed73915bd4dacabce6aebc1d1
Daniel Johnson <voltron42@users.noreply.github.com>
2024-06-21 16:09:03 +0000

    adding winner check logic

--- catchpenny-colonnade/infinitic/infinitic.js
+++ catchpenny-colonnade/infinitic/infinitic.js
@@ -2,6 +2,33 @@ namespace("infinitic.Infinitic",{},() => {
   let colors = {
     dark: "#212529"
   };
+  let winners = [
+    "012",
+    "345",
+    "678",
+    "036",
+    "147",
+    "258",
+    "246",
+    "048"
+  ]
+  let hasWinner = function(spaces, turn) {
+    let places = spaces.filter((_,i) => i % 2 === 0);
+    if (places.length < 3) {
+      return ;
+    }
+    places.sort();
+    let possibles = [];
+    if (places.length === 3) {
+      possibles.push(places.join(""));
+    } else {
+      possibles = places.map(p => places.join("").replaceAll(p.toString(), ""));
+    }
+    let wins = possibles.map(p => winners.indexOf(p)).filter(w => w > 0);
+    if (wins.length > 0) {
+      return winners[wins[0]];
+    }
+  }
   return class extends React.Component {
     constructor(props){
       super(props);
@@ -15,13 +42,21 @@ namespace("infinitic.Infinitic",{},() => {
       return (index < 0)?"":(((index % 2 == 0) == this.state.turn)?"X":"O");
     }
     clickCell(space) {
-      var spaces = Array.from(this.state.spaces);
-      spaces.unshift(space);
-      if (spaces.length > 6) {
-        spaces.pop();
+      let icon = this.getIcon(space);
+      if (icon != "") {
+        let spaces = Array.from(this.state.spaces);
+        spaces.unshift(space);
+        let winner = getWinner(spaces);
+        if (spaces.length > 6) {
+          spaces.pop();
+        }
+        console.log(this.state);
+        let update = { spaces, turn: !this.state.turn };
+        if (winner) {
+          update.winner = winner;
+        }
+        this.setState();
       }
-      console.log(this.state)
-      this.setState({ spaces, turn: !this.state.turn });
     }
     render() {
       return (<div className="d-flex justify-content-center align-items-center">
