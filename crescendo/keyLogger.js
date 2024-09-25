c638e5c0038daf2f405fe22b3ab5c0f3866f9b95
Daniel Johnson <voltron42@users.noreply.github.com>
2024-09-25 20:42:44 +0000

    updating crescendo project

---
+++ crescendo/keyLogger.js
@@ -0,0 +1,14 @@
+namespace("crescendo.KeyLogger", () => {
+  return function() {
+    const keyLogger = {};
+    document.addEventListener("keyhold",({detail}) => {
+      keyLogger[detail.code] = true;
+    });
+    document.addEventListener("keyrelease",({detail}) => {
+      delete keyLogger[detail.code];
+    });
+    return function() {
+      return Object.keys(keyLogger);
+    }
+  }
+})
