c196d5851ab2a1becbf9bbd1c4967c49e3d17aba
Voltron42 <dajohnson1978@gmail.com>
2024-09-27 16:17:48 -0500

    gamepad test completed

--- crescendo/gamepad.js
+++ crescendo/gamepad.js
@@ -1,54 +1,63 @@
 (function(){
   const DEFAULT_AXIS_THRESHOLD = 0.05;
   const gamepads = {};
-  const dispatcher = function(buttonLabels,state,gamepadIndex,singular,getter) {
+  const dispatcher = function(buttonLabels, state, gamepadType, gamepadIndex, singular, getter) {
     return function(item, index) {
       const newValue = getter(item);
       const oldValue = state[index];
+      const label = buttonLabels[index];
       if (oldValue != newValue) {
-        const label = buttonLabels[index];
         if (!newValue) {
-          window.dispatchEvent(new CustomEvent(`gamepad${singular}released`,{ detail: { gamepadIndex, index, label }}));
-          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${label}released`));
-          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${index}released`));
+          window.dispatchEvent(new CustomEvent(`gamepad${singular}released`,{ detail: { gamepadIndex, gamepadType, index, label }}));
+          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${label}released`, { detail: { gamepadType }}));
+          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${index}released`, { detail: { gamepadType }}));
         } else if (!oldValue) {
-          window.dispatchEvent(new CustomEvent(`gamepad${singular}triggered`,{ detail: { gamepadIndex, index, label, newValue }}));
-          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${label}triggered`,{ detail: newValue }));
-          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${index}triggered`,{ detail: newValue }));
+          window.dispatchEvent(new CustomEvent(`gamepad${singular}triggered`,{ detail: { gamepadIndex, gamepadType, index, label, newValue }}));
+          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${label}triggered`,{ detail: { gamepadType, newValue }}));
+          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${index}triggered`,{ detail: { gamepadType, newValue }}));
         } else {
-          window.dispatchEvent(new CustomEvent(`gamepad${singular}changed`,{ detail: { gamepadIndex, index, label, newValue }}));
-          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${label}changed`,{ detail: newValue }));
-          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${index}changed`,{ detail: newValue }));
+          window.dispatchEvent(new CustomEvent(`gamepad${singular}changed`,{ detail: { gamepadIndex, gamepadType, index, label, newValue }}));
+          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${label}changed`,{ detail: { gamepadType, newValue }}));
+          window.dispatchEvent(new CustomEvent(`gamepad${gamepadIndex}${singular}${index}changed`,{ detail: { gamepadType, newValue }}));
         }
         state[index] = newValue;
       }
     };
   }
-  const pollGamepad = function(buttonLabels,state,{axes,buttons,timestamp},gamepadIndex,axisThreshold) {
+  const pollGamepad = function(buttonLabels,state,{id,axes,buttons,timestamp},gamepadIndex,axisThreshold) {
     if (timestamp > state.timestamp) {
-      axes.forEach(dispatcher(buttonLabels.axes, state.axes, gamepadIndex,"axis",(a) => {
+      state.timestamp = timestamp;
+      buttonLabels = buttonLabels || { axes: [], buttons: [] };
+      axes.forEach(dispatcher(buttonLabels.axes, state.axes, id, gamepadIndex, "axis", (a) => {
         const newValue = Math.round(a*100)/100;
-        return (Math.abs(newValue) > axisThreshold)?axisThreshold:undefined;
+        return (Math.abs(newValue) > axisThreshold)?newValue:undefined;
       }));
-      buttons.forEach(dispatcher(buttonLabels.buttons, state.buttons, gamepadIndex, "button",(b) => (b.pressed || b.touched)?b.value:undefined));
+      buttons.forEach(dispatcher(buttonLabels.buttons, state.buttons, id, gamepadIndex, "button", (b) => (b.pressed || b.touched)?b.value:undefined));
     }
   }
-  window.initGamepad = function(buttonLabels,axisThreshold) {
+  const clearGamepad = function(index) {
+    clearInterval(gamepads[index]);
+    delete gamepads[index];
+  }
+  window.initGamepad = function(buttonLabels, axisThreshold) {
     axisThreshold = isNaN(axisThreshold)?DEFAULT_AXIS_THRESHOLD:axisThreshold;
     buttonLabels = buttonLabels || {};
     window.addEventListener("gamepaddisconnected", (e) => {
-      clearInterval(gamepads[e.gamepad.index]);
-      delete gamepads[e.gamepad.index];
+      clearGamepad(e.gamepad.index);
     });
     window.addEventListener("gamepadconnected", (e) => {
       console.log({ gamepad: e.gamepad });
-      const state = {axes:[],buttons:[]};
+      const state = { axes: [], buttons: [], timestamp: e.gamepad.timestamp };
       gamepads[e.gamepad.index] = {
         state,
         interval: setInterval(() => {
-          const gamepad = navigator.getGamepads()[e.gamepad.index]
-          pollGamepad(buttonLabels[gamepad.id],state,gamepad,e.gamepad.index,axisThreshold);
-        },50)
+          const gamepad = navigator.getGamepads()[e.gamepad.index];
+          if (gamepad) {
+            pollGamepad(buttonLabels[gamepad.id], state, gamepad, e.gamepad.index, axisThreshold);
+          } else {
+            clearGamepad(e.gamepad.index);
+          }
+        },10)
       };
     });
   }
---
+++ crescendo/gamepadtest.js
@@ -0,0 +1,134 @@
+namespace("crescendo.GamepadTest", () => {
+  const buttonLabels = {
+    "Xbox 360 Controller (XInput STANDARD GAMEPAD)": {
+      axes: "Lx,Ly,Rx,Ry".split(","),
+      buttons: "A,B,X,Y,Ls,Rs,Lt,Rt,Select,Start,L3,R3,DPadUp,DPadDown,DPadLeft,DPadRight,Menu".split(",")
+    },
+    "Generic   USB  Joystick   (Vendor: 0079 Product: 0006)": {
+      axes: ["joystickX", "joystickY"],
+      buttons: "red1,yellow1,green1,blue1,red2,yellow2,green2,blue2,coin,player1".split(",")
+    }
+  };
+  const standardButtons = "A,B,X,Y,Ls,Rs,Select,Start,L3,R3,DPadUp,DPadDown,DPadLeft,DPadRight,Menu,red1,yellow1,green1,blue1,red2,yellow2,green2,blue2,coin,player1".split(",");
+  const joystickDirections = {
+    "X": ["W","","E"],
+    "Y": ["N","","S"],
+  }
+  const allJoystickDirections = joystickDirections.Y.reduce((outval, x) => {
+    return outval.concat(joystickDirections.X.map(y => x + y));
+  }, []).filter(d => d.length > 0);
+  const buttonColors = {
+    red1: "red",
+    yellow1: "yellow",
+    green1: "green",
+    blue1: "blue",
+    red2: "red",
+    yellow2: "yellow",
+    green2: "green",
+    blue2: "blue",
+    B: "red",
+    Y: "yellow",
+    A: "green",
+    X: "blue",    
+  }
+  const eventTypes = "triggered,changed".split(",");
+  const actionTypes = "axis,button".split(",");
+  const events = eventTypes.reduce((outVal, eventType) => {
+    return outVal.concat(actionTypes.map(actionType => `gamepad${actionType}${eventType}`));
+  }, []);
+  const dPad = {
+    "joystickX": true,
+    "joystickY": true,
+  };
+  const init = function() {
+    initGamepad(buttonLabels);
+    console.log("init");
+    console.log({ events });
+    const states = {
+    };
+    events.forEach((eventName) => {
+      window.addEventListener(eventName, ({ detail }) => {
+        var { newValue, label } = detail;
+        states[label] = newValue;
+      });
+    });
+    actionTypes.forEach((actionType) => {
+      const eventName = `gamepad${actionType}released`;
+      window.addEventListener(eventName, ({ detail }) => {
+        var { label } = detail;
+        states[label] = 0;
+      });
+    });
+    const getAllAttributes = function(frameElem) {
+      return function(retVal, attr) {
+        retVal[attr] = parseFloat(frameElem.getAttribute(attr));
+        return retVal;
+      };
+    }
+    const updateStick = function(frameId, stickId, xAxis, yAxis) {
+      const frame = document.getElementById(frameId);
+      const stick = document.getElementById(stickId);
+      const frameArgs = ["cx","cy","r"].reduce(getAllAttributes(frame), {});
+      const stickRadius = getAllAttributes(stick)({}, "r").r;
+      const mapping = {
+        "cx": xAxis,
+        "cy": yAxis
+      };
+      const inputs = Object.entries(mapping).reduce((outVal, [arg, axis]) => {
+        outVal[arg] = (states[axis] || 0);
+        return outVal;
+      }, {});
+      const range = frameArgs.r - stickRadius;
+      Object.entries(inputs).forEach(([arg, value]) => {
+        stick.setAttribute(arg, frameArgs[arg] + (range * value));
+      });
+    };
+    const updateTrigger = function(frameId, barId, triggerKey) {
+      document.getElementById(barId).setAttribute("width", document.getElementById(frameId).getAttribute("width") * (states[triggerKey] || 0));
+    }
+    setInterval(() => {
+      standardButtons.forEach((button) => {
+        const element = document.getElementById(button)
+        if (element) {
+          element.setAttribute("fill",((states[button] || 0)?(buttonColors[button]||"white"):"none"))
+        } else {
+          console.log({ msg: "button not found", button });
+        }
+      });
+      const currentJoystickDirection = allJoystickDirections.filter((dir) => {
+        const id = `joystick${dir}`;
+        const element = document.getElementById(id);
+        if (element) {
+          return element.getAttribute("fill") != "none";
+        } else {
+          console.log({ msg: "joystick not found", id });
+          return false;
+        }
+      })[0];
+      const joystickDirection = ["Y","X"].map(d => {
+        return joystickDirections[d][(states[`joystick${d}`] || 0) + 1];
+      }).join("");
+      if (joystickDirection.length > 0) {
+        const element = document.getElementById("joystick" + joystickDirection);
+        if (element) {
+          element.setAttribute("fill","white");
+        } else {
+          console.log({ msg: "joystick not found", joystickDirection });
+        }
+      }
+      if (currentJoystickDirection != undefined && currentJoystickDirection != joystickDirection) {
+        const element = document.getElementById("joystick" + currentJoystickDirection);
+        if (element) {
+          element.setAttribute("fill","none");
+        } else {
+          console.log({ msg: "joystick not found", currentJoystickDirection });
+        }
+      }
+      updateStick("frameL", "stickL", "Lx", "Ly");
+      updateStick("frameR", "stickR", "Rx", "Ry");
+      updateTrigger("LtFrame", "LtBar", "Lt");
+      updateTrigger("RtFrame", "RtBar", "Rt");
+    }, 42);
+  }
+  return { init };
+});
