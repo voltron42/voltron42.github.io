fb2eea83cc2f421967ae919bd755fab80564c8b1
Daniel Johnson <voltron42@users.noreply.github.com>
2024-10-02 16:30:16 +0000

    building out details on board, stubbing javascript

---
+++ 2181robotics/beach-bash/beach-bash.js
@@ -0,0 +1,10 @@
+namespace("2181robotics.beach-bash.BeachBash", {}, ({}) => {
+  return function() {
+    this.init = function() {
+      
+    }
+    this.config = function(event) {
+
+    }
+  }
+});
