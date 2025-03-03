b12edb279cd8984dabb305a737da8ea567558dd1
Daniel Johnson <voltron42@users.noreply.github.com>
2025-03-03 19:07:50 +0000

    removing gizmo atheneum into own org and repo

--- gizmo-atheneum/extensions/EventTarget/eventtarget.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  EventTarget.prototype.addEventListeners = function(listenerMap) {
-    var me = this;
-    Object.entries(listenerMap).forEach((entry) => {
-      me.addEventListener(entry[0],entry[1]);
-    });
-  };
-})();
